b7ddbd84ddc35862055de0b5a5181111a09a746e
MahnurA <ua.runham@gmail.com>
2019-05-26 17:59:33 +0500

    updated

--- EnsembleModeling.py
+++ EnsembleModeling.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np 
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
@@ -26,150 +20,55 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from ipykernel import kernelapp as app
 
-
 # for clearing warnings
 import warnings
 warnings.filterwarnings("ignore")
 
-
-# In[99]:
-
-
-train = pd.read_csv('C:\\Users\sunil\OneDrive\Documents\FYP\Train\myTrain.csv', nrows= 3020 , low_memory=False)
-test = pd.read_csv('C:\\Users\sunil\OneDrive\Documents\FYP\Test\myTest.csv',nrows = 510, low_memory=False)
-
-
-# In[100]:
-
+train = pd.read_csv('dir\path\myTrain.csv', nrows= 3020 , low_memory=False)
+test = pd.read_csv('dir\path\myTest.csv',nrows = 510, low_memory=False)
 
 #Drop these features from train and test:
 train = train.drop(['Flow_ID', 'Src_IP' , 'Dst_IP', 'Timestamp'], axis = 1) 
 test = test.drop(['Flow_ID', 'Src_IP' , 'Dst_IP', 'Timestamp'], axis = 1) 
 
-
-#Check em datatypes
-#train.dtypes
-
-
-# In[101]:
-
-
-#train.head(50)
-#train_y.head()
-#DROP THEM NULL ROWS
-#train.replace(["NaN", 'NaT'], np.nan, inplace = True)
-
 train = train.dropna()
 test = test.dropna()
-
-
-# In[91]:
-
-
 print(train.shape)
 print(test.shape)
 
-
-# In[102]:
-
-
 #X has features in it, and Y has labels
-#train_y = train.loc[:, train.columns == 'Label']
-#train_x = train.iloc[:,-1]
-#train_y = train.iloc[:,1]
-#train_x = train.iloc[:,1:]
 train_x = train.loc[:, train.columns != 'Label']
 train_y = train.iloc[:,-1]
 
 print(train_x.shape)
-#print(train_y.shape)
-#train_y.head()
-#train_x.head
-
-
-# In[103]:
-
 
 test_x = test.loc[:, test.columns != 'Label']
-#test_y = test.loc[:, test.columns == 'Label']
 #test_x = test.iloc[:,1:]
 test_y = test.iloc[:,-1]
 
-
-#test_x = test.iloc[:,1:]
-#test_y = test.iloc[:,0]
-
-
-# In[104]:
-
-
 #Check for Null values
 train_x.isnull().values.any()
-#train_x.isnull().sum().sum()
-
-#train_x.isna().any()
-#train_x.loc[:, train_x.isna().any()]
-
-#test_x.loc[:, test_x.isna().any()]
-
-
-# In[9]:
-
 
 #DATA PREPOCESSING
 #Remove them constants features from train n test
 constant_filter = VarianceThreshold(threshold=0.3)  
 constant_filter.fit(train_x) 
 len(train_x.columns[constant_filter.get_support()]) 
-
-
-# In[10]:
-
-
 constant_columns = [column for column in train_x.columns  
                     if column not in train_x.columns[constant_filter.get_support()]]
 
 print(len(constant_columns))  
 
-
-# In[11]:
-
-
 #Print them constant features
 for column in constant_columns:  
     print(column)
-
-
-# In[94]:
-
-
-#Remove them constants features from train n test
-#train_x = constant_filter.transform(train_x)  
-#test_x = constant_filter.transform(test_x)
-
-#train_x.shape, test_x.shape  
-#train_x.head
-#train_x = train_x.drop([constant_columns]) 
-#test_x = test_x.drop([constant_columns]) 
-
-#for column in train_x.columns: 
-#    if [constant_columns] in train_x[col] : 
-#            del train_x[col] 
             
 print(train_x.shape)
-
-
-# In[105]:
-
 
 #TO REMOVE CORRELATED FEATURES
 correlated_features = set()  
 correlation_matrix = train_x.corr()  
 correlation_matrix = test_x.corr()  
-
-
-# In[106]:
-
 
 for i in range(len(correlation_matrix .columns)):  
     for j in range(i):
@@ -177,16 +76,8 @@ for i in range(len(correlation_matrix .columns)):
             colname = correlation_matrix.columns[i]
             correlated_features.add(colname)
 
-
-# In[107]:
-
-
 len(correlated_features)  
 print(correlated_features)  
-
-
-# In[108]:
-
 
 train_x.drop(labels=correlated_features, axis=1, inplace=True)  
 test_x.drop(labels=correlated_features, axis=1, inplace=True)  
@@ -195,87 +86,30 @@ test_x.drop(labels=correlated_features, axis=1, inplace=True)
 #print(train_x.head(2)) 
 train_x.dtypes
 
-
-# In[109]:
-
-
 #descision tree
 dt = DecisionTreeClassifier()
 dt.fit(train_x,train_y)
-
-
-# In[51]:
-
-
 dt.score(test_x,test_y)
-
-
-# In[52]:
-
-
 dt.score(train_x,train_y)
 
-
-# In[53]:
-
-
 #Random Forest - Ensemble of Descision Trees
-
 rf = RandomForestClassifier(n_estimators=20)
 rf.fit(train_x,train_y)
-
-
-# In[54]:
-
-
 rf.score(test_x,test_y)
 
-
-# In[55]:
-
-
 #Bagging --> Overfitting less compared to ada boosting
-
 bg = BaggingClassifier(DecisionTreeClassifier(), max_samples= 0.8, max_features = 0.5, n_estimators = 20)
 bg.fit(train_x,train_y)
-
-
-# In[56]:
-
-
 bg.score(test_x,test_y)
-
-
-# In[57]:
-
-
 bg.score(train_x,train_y)
 
-
-# In[58]:
-
-
 #Boosting - Ada Boost
-
 adb = AdaBoostClassifier(DecisionTreeClassifier(),n_estimators = 5, learning_rate = 1)
 adb.fit(train_x,train_y)
-
-
-# In[59]:
-
-
 adb.score(test_x,test_y)
-
-
-# In[60]:
-
 
 #Clearly over-fitting
 adb.score(train_x,train_y)
-
-
-# In[61]:
-
 
 # Voting Classifier - Multiple Model Ensemble 
 
@@ -283,33 +117,10 @@ lr = LogisticRegression()
 dt = DecisionTreeClassifier()
 svm = SVC(kernel = 'poly', degree = 4 )
 
-
-# In[62]:
-
-
 evc = VotingClassifier( estimators= [('lr',lr),('dt',dt),('svm',svm)], voting = 'hard')
-
-
-# In[63]:
-
-
 evc.fit(train_x.iloc[1:3000],train_y.iloc[1:3000])
-
-
-# In[64]:
-
-
 evc.score(test_x, test_y)
-
-
-# In[65]:
-
-
 evc.score(train_x, train_y)
-
-
-# In[36]:
-
 
 #print(test_y,)
 from yellowbrick.classifier import ClassificationReport
@@ -320,4 +131,3 @@ visualizer = ClassificationReport(evc, classes=['xss','ssh','portscan','Benign']
 visualizer.fit(train_x, train_y)  # Fit the training data to the visualizer
 visualizer.score(test_x, test_y)  # Evaluate the model on the test data
 g = visualizer.poof()             # Draw/show/poof the data
-
--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # SAD
 FYP Fall 2018:
+
+This project lies on the intersection between Network Security and Machine Learning. Most Intrusion Detection Systems (IDS) today, work on signature based detection which results in new variants of network attacks being undetected until their signatures are updated into the system. IDS like these cannot stop a network attack whose signature is not in its database. To rectify this, instead of relying on a database for signatures, we have created a machine learning model that can classify new variants of attacks instantly. This is done by training the model on a number of classes of attacks, after which it can, detect new variants of attacks within those classes. 
+
+The model here, is trained on Portscan, BruteForceSSH, Cross-site Scripting, SQL Injection, and Denial of Service attacks. New variations within these attacks are successfully detected by the model with an accuracy of 0.96%. These classes of attacks were chosen as they are one of the most common types of attack as according to OWASP, symantec and numerous other surveys and [reports](https://www.symantec.com/security-center/threat-report). 
+
+Both training data and test data was generated in the Robotics Lab at IBA City campus, the attacks were carried out in an isolated lab environment that we set up. [Wireshark](https://www.wireshark.org) was used to capture this data into pcap files through a SPAN port on the Cisco switch. These are converted to CSV files by [CICFlowMeter](https://www.unb.ca/cic/research/applications.html#CICFlowMeter), feature extraction from the pcaps is also done here. 
+
+Variations within each attack were confirmed and tested by running them against [Snort IDS/IPS](https://www.snort.org). Each attack has atleast one or more variatons that could not be detected by Snort but can be detected by our model. 
+
+This model has been integrated with [TCPDump](https://www.tcpdump.org) and [CICFlowMeter](https://www.unb.ca/cic/research/applications.html#CICFlowMeter) so that the finished product can capture traffic, convert the captured traffic from Pcap to CSV, feed it into the model and classify the captured traffic as malicious or benign.   
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine. To run the application, please 
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* [TCPDump](https://www.tcpdump.org) - The portable C/C++ library used for network traffic capture.
+* [CICFlowMeter](https://www.unb.ca/cic/research/applications.html#CICFlowMeter) - The network traffic flow generator and analyser.
+* [Sci-kit learn](https://scikit-learn.org/stable/) -  Python tool for data mining and data analysis, built on NumPy, SciPy, and matplotlib libraries.
+
+
+## Authors
+
+* **Syeda Mahnur Asif** - [MahnurA](https://github.com/PurpleBooth)
+* **Sunila Aftab** - [sunila-aftab](https://github.com/sunila-aftab)
+* **Emaan Hasan** - [emaan-hasan](https://github.com/emaan-hasan)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE.md](LICENSE.md) file for details
+
+
+
+
